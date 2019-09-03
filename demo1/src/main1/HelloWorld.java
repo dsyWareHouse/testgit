a3e49c017fcf68550e56c3f9b723f42af0fb4efc
git <995958125@qq.com>
2019-09-03 11:21:46 +0800

    首次上传demo1

---
+++ demo1/src/main1/HelloWorld.java
@@ -0,0 +1,10 @@
+package main1;
+
+public class HelloWorld {
+	
+	public static void main(String[] args){
+		System.out.println("HelloWorld!!!");
+		
+	}
+
+}
---
+++ demo1/src/test1/HelloWorld.java
@@ -0,0 +1,10 @@
+package test1;
+
+public class HelloWorld {
+	
+	public static void main(String[] args){
+		System.out.println("HelloWorld!!!");
+		
+	}
+
+}
